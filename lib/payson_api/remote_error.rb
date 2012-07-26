12b431fcff351fff2f57b3c5c937cc50dc0083e6
Christopher Svensson <stoffus@stoffus.com>
2012-07-26 16:35:38 +0200

    Bug fix: Strictly one parameter per error object

--- lib/payson_api/remote_error.rb
+++ lib/payson_api/remote_error.rb
@@ -2,13 +2,13 @@ require 'cgi'
 
 module PaysonAPI
 class RemoteError
-  attr_accessor :id, :message, :parameters
+  attr_accessor :id, :message, :parameter
   FORMAT_STRING = "errorList.error(%d).%s"
 
-  def initialize(id, message, parameters)
+  def initialize(id, message, parameter)
     @id = id
     @message = message
-    @parameters = parameters
+    @parameter = parameter
   end
 
   def self.parse(data)
@@ -17,15 +17,15 @@ class RemoteError
       while data[FORMAT_STRING % [i, 'errorId']]
         id = data[FORMAT_STRING % [i, 'errorId']]
         message = CGI.unescape(data[FORMAT_STRING % [i, 'message']])
-        parameters = CGI.unescape(data[FORMAT_STRING % [i, 'parameter']])
-        errors << self.new(id, message, parameters)
+        parameter = CGI.unescape(data[FORMAT_STRING % [i, 'parameter']])
+        errors << self.new(id, message, parameter)
         i += 1
       end
     end
   end
 
   def to_s
-    "ID: #{@id}, Message: #{@message}, Parameters: #{@parameters}"
+    "ID: #{@id}, Message: #{@message}, Parameter: #{@parameter}"
   end
 end
 end
