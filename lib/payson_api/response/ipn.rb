d3fc579fc9c84c77b54c662e8370ccd43da8f2da
Christopher Svensson <stoffus@stoffus.com>
2012-07-27 16:41:16 +0200

    Bug fix: IPN response class will be initialized with a string, not a hash

--- lib/payson_api/response/ipn.rb
+++ lib/payson_api/response/ipn.rb
@@ -4,30 +4,31 @@ class IPN
   attr_accessor :purchase_id, :sender_email, :status,
     :payment_type, :guarantee_status, :guarantee_deadline_at,
     :invoice_status, :custom, :tracking_id, :receivers, :currency,
-    :order_items, :fundings, :token, :shipping_address
-    :raw
+    :order_items, :fundings, :token, :shipping_address, :raw, :hash
 
-  def initialize(data)
-    @raw = data
-    @purchase_id = data['purchaseId']
-    @payment_type = data['type']
-    @comment = data['custom']
-    @tracking_id = data['trackingId']
-    @currency = data['currencyCode']
-    @sender_email = data['senderEmail']
-    @status = data['status']
-    @token = data['token']
-    @fundings = Funding.parse(data)
-    @receivers = Receiver.parse(data)
-    @order_items = OrderItem.parse(data)
+  def initialize(raw_data)
+    @raw = raw_data
+    data_hash = PaysonAPI::Client.params_to_hash(raw_data)
+    @purchase_id = data_hash['purchaseId']
+    @payment_type = data_hash['type']
+    @comment = data_hash['custom']
+    @tracking_id = data_hash['trackingId']
+    @currency = data_hash['currencyCode']
+    @sender_email = data_hash['senderEmail']
+    @status = data_hash['status']
+    @token = data_hash['token']
+    @fundings = Funding.parse(data_hash)
+    @receivers = Receiver.parse(data_hash)
+    @order_items = OrderItem.parse(data_hash)
+    @hash = data_hash['HASH']
 
     case @payment_type
     when 'GUARANTEE'
-      @guarantee_status = data['guaranteeStatus']
-      @guarantee_deadline_at = Time.at(data['guaranteeDeadlineTimestamp'])
+      @guarantee_status = data_hash['guaranteeStatus']
+      @guarantee_deadline_at = Time.at(data_hash['guaranteeDeadlineTimestamp'])
     when 'INVOICE'
-      @invoice_status = data['invoiceStatus']
-      @shipping_address = ShippingAddress.parse(data)
+      @invoice_status = data_hash['invoiceStatus']
+      @shipping_address = ShippingAddress.parse(data_hash)
     end
   end
 end
