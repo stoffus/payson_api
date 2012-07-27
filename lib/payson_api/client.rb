6bde1da8c0e85281d4ca6a8fc11fb32d0f200a4c
Christopher Svensson <stoffus@stoffus.com>
2012-07-27 16:40:18 +0200

    Fixes in Client class to be compatible with IPN request

--- lib/payson_api/client.rb
+++ lib/payson_api/client.rb
@@ -3,62 +3,38 @@ require 'net/https'
 module PaysonAPI
 class Client
   def self.initiate_payment(payment_data)
-    response_hash = payson_request(
+    response = payson_request(
       PAYSON_API_PAY_ACTION,
       payment_data.to_hash
     )
+    response_hash = params_to_hash(response.body)
     PaysonAPI::Response::Payment.new(response_hash)
   end
 
   def self.get_payment_details(payment_details_data)
-    response_hash = payson_request(
+    response = payson_request(
       PAYSON_API_PAYMENT_DETAILS_ACTION,
       payment_details_data.to_hash
     )
+    response_hash = params_to_hash(response.body)
     PaysonAPI::Response::PaymentDetails.new(response_hash)
   end
 
   def self.update_payment(payment_update_data)
-    response_hash = payson_request(
+    response = payson_request(
       PAYSON_API_PAYMENT_DETAILS_ACTION,
       payment_update_data.to_hash
     )
+    response_hash = params_to_hash(response.body)
     PaysonAPI::Response::PaymentUpdate.new(response_hash)
   end
 
   def self.validate_ipn(ipn_data)
-    response_hash = payson_request(
+    response = payson_request(
       PAYSON_API_PAYMENT_VALIDATE_ACTION,
       ipn_data.to_s
     )
-    PaysonAPI::Response::IPN.new(response_hash)
-  end
-
-private
-
-  def self.payson_request(action, data)
-    action = '/%s/%s/' % [PAYSON_API_VERSION, action]
-    url = PAYSON_API_ENDPOINT + action
-    headers = {
-      'PAYSON-SECURITY-USERID' => PaysonAPI.config.api_user_id,
-      'PAYSON-SECURITY-PASSWORD' => PaysonAPI.config.api_password,
-      'Content-Type' => 'application/x-www-form-urlencoded'
-    }
-    response = post(url, data, headers)
-    params_to_hash(response.body)
-  end
-
-  def self.post(url, data, headers)
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.use_ssl = uri.scheme == 'https'
-    req = Net::HTTP::Post.new(uri.path)
-    req.body = hash_to_params(data)
-    headers.each do |name, value|
-      req[name] = value
-    end
-    http.request(req)
+    PaysonAPI::Response::Validate.new(response.body)
   end
 
   def self.hash_to_params(hash)
@@ -75,6 +51,32 @@ private
         hash[key] = val
       end
     end
+  end
+
+private
+
+  def self.payson_request(action, data)
+    action = '/%s/%s/' % [PAYSON_API_VERSION, action]
+    url = PAYSON_API_ENDPOINT + action
+    headers = {
+      'PAYSON-SECURITY-USERID' => PaysonAPI.config.api_user_id,
+      'PAYSON-SECURITY-PASSWORD' => PaysonAPI.config.api_password,
+      'Content-Type' => 'application/x-www-form-urlencoded'
+    }
+    post(url, data, headers)
+  end
+
+  def self.post(url, data, headers)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http.use_ssl = uri.scheme == 'https'
+    req = Net::HTTP::Post.new(uri.path)
+    req.body = data.is_a?(Hash) ? hash_to_params(data) : data
+    headers.each do |name, value|
+      req[name] = value
+    end
+    http.request(req)
   end
 end
 end
