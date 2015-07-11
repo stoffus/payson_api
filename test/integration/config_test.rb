4db1fe59aab7005dabf0989bbc703fd8a5bb0537
Gustav <gustav@invoke.se>
2015-07-11 11:59:17 +0200

    Add config variable for test_mode
    If you have your own test agent you should be able to enable test mode.

--- test/integration/config_test.rb
+++ test/integration/config_test.rb
@@ -12,4 +12,12 @@ class ConfigTest < Test::Unit::TestCase
     assert_equal CONFIG[:api_password], PaysonAPI.config.api_password
     assert PaysonAPI.test?
   end
+
+  def test_setting_test_mode
+    PaysonAPI.config.api_user_id = '123'
+    assert !PaysonAPI.test?
+    PaysonAPI.config.test_mode = true
+    assert PaysonAPI.test?
+  end
+
 end
